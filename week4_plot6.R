fe542c3f8978613f58929f635d7d48665fbc9429
Gilda M Castro Sancho <59642399+gmcastros@users.noreply.github.com>
2020-03-28 21:37:44 +0100

    corrected the delta calculation

--- week4_plot6.R
+++ week4_plot6.R
@@ -7,16 +7,12 @@ SCC <- readRDS("Source_Classification_Code.rds")
 #Compare emissions from motor vehicle sources in Baltimore City with emissions from motor vehicle sources in Los Angeles County, California (fips == "06037"\color{red}{\verb|fips == "06037"|}fips=="06037"). 
 #Which city has seen greater changes over time in motor vehicle emissions?
 
-california_data <- NEI[NEI$fips=="06037", ]
-baltimore_data <- NEI[NEI$fips=="24510", ]
 
 #Get the coal sources, the column EI.Sector has the information to filter
 vehicles <- SCC[grep("Veh", SCC$Short.Name),] 
 
 vehicles_1 <- subset( NEI, NEI$SCC %in% vehicles$SCC)
 vehicles_all <- merge(x = vehicles_1, y = SCC, by.x="SCC", by.y="SCC")
-
-
 
 #We need to group the data at year level. SUM it. 
 # The columns we need are Emissions and year 
@@ -29,13 +25,21 @@ emissions_per_year <- rbind(emissions_per_year_baltimore, emissions_per_year_cal
 #Use ggplot
 library(ggplot2)
 #draw emissions per year, over year, emissions and color by type, each color line.
-plot5 <- ggplot(emissions_per_year, aes(year, Emissions, color=fips))
-plot5 <- plot5 + geom_line() + xlab("Year") + ylab("Total Emissions")
-png("plot5.png", width=480, height = 480)
-print(plot5)
+plot6 <- ggplot(emissions_per_year, aes(year, Emissions, color=fips))
+plot6 <- plot6 + geom_line() + xlab("Year") + ylab("Total Emissions")
+png("plot6.png", width=480, height = 480)
+print(plot6)
 dev.off()
 
-emissions_2008 <- emissions_per_year[emissions_per_year$year ==2008,2]
-emissions_1999 <- emissions_per_year[emissions_per_year$year ==1999,2]
-emissions_delta_2008_1999 <- emissions_2008 - emissions_1999
+emissions_2008_baltimore <- emissions_per_year_baltimore[emissions_per_year_baltimore$year ==2008,3]
+emissions_1999_baltimore <- emissions_per_year_baltimore[emissions_per_year_baltimore$year ==1999,3]
 
+emissions_2008_california <- emissions_per_year_california[emissions_per_year_california$year ==2008,3]
+emissions_1999_california <- emissions_per_year_california[emissions_per_year_california$year ==1999,3]
+
+california_delta <- emissions_2008_california - emissions_1999_california
+baltimore_delta <- emissions_2008_baltimore - emissions_1999_baltimore
+
+#california delta: 163.44
+#baltimore delta: -258.5445
+#Baltimore city experienced the biggest change. it decreased -258.5445
